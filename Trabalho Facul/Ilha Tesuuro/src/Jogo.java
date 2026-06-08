d057d42e3d734b077ed051093024dfe91e9bc654
João Victor Reinert <162636975+ReinertJao@users.noreply.github.com>
2026-06-08 10:27:25 -0300

    Ajustes

--- Trabalho Facul/Ilha Tesuuro/src/Jogo.java
+++ Trabalho Facul/Ilha Tesuuro/src/Jogo.java
@@ -1,40 +1,57 @@
 public class Jogo {
     /*
-     * Item encontrado Tipo          Pontuação
-     * "OURO"        Tesouro        +10 pontos
-     * "DIAMANTE"    Tesouro        +20 pontos
-     * "RUBI"        Tesouro        +15 pontos
+     * Item encontrado Tipo Pontuação
+     * "OURO" Tesouro +10 pontos
+     * "DIAMANTE" Tesouro +20 pontos
+     * "RUBI" Tesouro +15 pontos
      * 
-     * "BURACO"      Armadilha       -5 pontos
-     * "COBRA"       Armadilha      -10 pontos
-     * "ESPINHOS"    Armadilha       -7 pontos
+     * "BURACO" Armadilha -5 pontos
+     * "COBRA" Armadilha -10 pontos
+     * "ESPINHOS" Armadilha -7 pontos
      * 
-     * "VAZIO"       Nenhum           0 pontos
+     * "VAZIO" Nenhum 0 pontos
      */
 
     public Jogo() {
+        
+        sortear();
+        
+        /*
+         * String[] tipos = {"VAZIO", "OURO", "DIAMANTE","RUBI", "BURADO", "COBRA",
+         * "ESPINHOS"};
+         * tipos[0]= "VAZIO";
+         * tipos[1] = "OURO";
+         * tipos[2] = "DIAMANTE";
+         * 
+         * int posicao = (int)(Math.random() * 7);
+         * System.out.println(posicao + " " + tipos[posicao]);
+         * // return posicao;
+         */
+
+        int ouro = 10;
+        int diamente = 20;
+        int rubi = 15;
+
+        int buraco = -5;
+        int cobra = -10;
+        int espinho = -7;
+
+        int vazio = 0;
+        
+        
+        // Vamos fazer por Assim, por que dps não precisa ficamr montando |
+    }
+    
+        private String sortear() {
         String[] tipos = {"VAZIO", "OURO", "DIAMANTE","RUBI", "BURADO", "COBRA", "ESPINHOS"};
         /*tipos[0]= "VAZIO";
         tipos[1] = "OURO";
         tipos[2] = "DIAMANTE";*/
 
         int posicao = (int)(Math.random() * 7);
-        System.out.println(posicao + " " + tipos[posicao]);
-        // return posicao;
-    
+        //System.out.println(posicao + " " + tipos[posicao]);
+        return tipos[posicao];
 
-        int ouro = 10;
-        int diamente = 20;
-        int rubi = 15;
-        
-        int buraco = -5;
-        int cobra = -10;
-        int espinho = -7;
-        
-        int vazio = 0;
-        
-        
-    }
     public static void main(String[] args) {
         new Jogo();
     }
