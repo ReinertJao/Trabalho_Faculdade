59ed745a04ef8ed186a9ac357cf9396bf355d36b
João Victor Reinert <162636975+ReinertJao@users.noreply.github.com>
2026-06-08 09:01:43 -0300

    P1

---
+++ Trabalho Facul/Ilha Tesuuro/src/Principal.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Principal {
+
+    public Principal() {
+        jogo jogo = new jogo();
+        
+        Scanner ler = new Scanner(System.in);
+        int ale;
+
+        System.out.println("===== ILHA DOS TESOUROS =====\r\n" + //
+                "1 - Mostrar instruções\r\n" + //
+                "2 - Mostrar mapa\r\n" + //
+                "3 - Jogar\r\n" + //
+                "4 - Mostrar status\r\n" + //
+                "5 - Sair");
+        ale = ler.nextInt();
+
+        switch (ale) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+                System.out.println("Sistema encerrado.");
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Principal();
+    }
+}
