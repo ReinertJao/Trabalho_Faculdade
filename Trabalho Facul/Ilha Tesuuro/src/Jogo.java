4367a08ce480f53c8d34eced3b5b32833ea2f810
João Victor Reinert <162636975+ReinertJao@users.noreply.github.com>
2026-06-15 09:49:20 -0300

    quase

--- Trabalho Facul/Ilha Tesuuro/src/Jogo.java
+++ Trabalho Facul/Ilha Tesuuro/src/Jogo.java
@@ -1,83 +1,130 @@
-
-
+import java.util.Scanner;
 
 public class Jogo {
-    
+    Scanner ler = new Scanner(System.in);
     /*
      * Item encontrado Tipo Pontuação
      * "OURO" Tesouro +10 pontos
      * "DIAMANTE" Tesouro +20 pontos
      * "RUBI" Tesouro +15 pontos
-     * 
+     *  
      * "BURACO" Armadilha -5 pontos
      * "COBRA" Armadilha -10 pontos
      * "ESPINHOS" Armadilha -7 pontos
      * 
      * "VAZIO" Nenhum 0 pontos
      */
-    
-    private String ilha[] = new String[15];
-    private boolean explorador[] = new boolean[15];
-    private int pontuação =0;
-    private int tentativas = 8;
 
-
+     String ilha[] = new String[15];
+     boolean explorador[] = new boolean[15];
+    int pontuacao = 0;
+    int tentativas = 8;
+    int tesourosEncontrados;
+    int armadilhasEncontradas;
+    int vaziosEncontrados;
     
-    void instrucoes(){
-     System.out.println(" Bem-vindo ao Island Pirates! \r\n" + //
-                  "\r\n" + //
-                  "Explore as ilhas em busca de tesouros, mas cuidado com as armadilhas!\r\n" + //
-                  "\r\n" + //
-                  "Você possui 8 tentativas e deve terminar o jogo com pontuação positiva.\r\n" + //
-                  "\r\n" + //
-                  " Tesouros:\r\n" + //
-                  " OURO -> +10 pontos\r\n" + //
-                  " RUBI -> +15 pontos\r\n" + //
-                  " DIAMANTE -> +20 pontos\r\n" + //
-                  "\r\n" + //
-                  " Armadilhas:\r\n" + //
-                  " BURACO -> -5 pontos\r\n" + //
-                  " ESPINHOS -> -7 pontos\r\n" + //
-                  " COBRA -> -10 pontos\r\n" + //
-                  "\r\n" + //
-                  " VAZIO -> 0 pontos\r\n" + //
-                  "\r\n" + //
-                  "Boa sorte, pirata! \r\n" + //
-                  "");   
+    
+    
+    
+    
+
+    public void instrucoes() {
+        System.out.println(" Bem-vindo ao Island Pirates! \r\n" + //
+                "\r\n" + //
+                "Explore as ilhas em busca de tesouros, mas cuidado com as armadilhas!\r\n" + //
+                "\r\n" + //
+                "Você possui 8 tentativas e deve terminar o jogo com pontuação positiva.\r\n" + //
+                "\r\n" + //
+                " Tesouros:\r\n" + //
+                " OURO -> +10 pontos\r\n" + //
+                " RUBI -> +15 pontos\r\n" + //
+                " DIAMANTE -> +20 pontos\r\n" + //
+                "\r\n" + //
+                " Armadilhas:\r\n" + //
+                " BURACO -> -5 pontos\r\n" + //
+                " ESPINHOS -> -7 pontos\r\n" + //
+                " COBRA -> -10 pontos\r\n" + //
+                "\r\n" + //
+                " VAZIO -> 0 pontos\r\n" + //
+                "\r\n" + //
+                "Boa sorte, pirata! \r\n" + //
+                "");
     }
+    
     
 
     // Contar de 0 a 14
-    private void mostrarMapa(int ilha[], boolean explorador[]) {
+    public void mostrarMapa(String ilha[], boolean explorador[]) {
         for (int i = 0; i < ilha.length; i++) {
-            System.out.println( ilha[i] + " ? ");
-            
+            ilha[i] = "?";
+
             if (explorador[i]) {
-                System.out.println("[" + i + "] Explorado" );
+                System.out.println("[" + i + "] Explorado");
             } else {
                 System.out.println("[" + i + "] ? ");
             }
         }
-    }
-    
-    private void jogando(){
         
     }
-    
-    private void mostrarStatus(int pontuação){
+
+    public void jogando(Scanner s) {
+        if (tentativas <= 0) {
+            System.out.println("Sem tentativas.");
+            return;
+        }
+        System.out.print("Escolha uma posição: ");
+        int posicao = s.nextInt();
+        if (posicao < 0 || posicao >= ilha.length) {
+            System.out.println("Posição inválida.");
+            return;
+        }
+        if (explorador[posicao]) {
+            System.out.println("Posição já explorada.");
+            return;
+        }
+        explorador[posicao] = true;
+        tentativas--;
+
+        String item = ilha[posicao];
+        if (item.equals("OURO")) {
+            pontuacao += 10;
+            tesourosEncontrados++;
+            System.out.println("Você encontrou OURO! +10");
+        } else if (item.equals("DIAMANTE")) {
+            pontuacao += 20;
+            tesourosEncontrados++;
+            System.out.println("Você encontrou DIAMANTE! +20");
+        } else if (item.equals("RUBI")) {
+            pontuacao += 15;
+            tesourosEncontrados++;
+            System.out.println("Você encontrou RUBI! +15");
+        } else if (item.equals("BURACO")) {
+            pontuacao -= 5;
+            armadilhasEncontradas++;
+            System.out.println("Você caiu em um BURACO! -5");
+        } else if (item.equals("COBRA")) {
+            pontuacao -= 10;
+            armadilhasEncontradas++;
+            System.out.println("Você encontrou uma COBRA! -10");
+        } else if (item.equals("ESPINHOS")) {
+            pontuacao -= 7;
+            armadilhasEncontradas++;
+            System.out.println("Você encontrou ESPINHOS! -7");
+        } else {
+            vaziosEncontrados++;
+            System.out.println("Posição vazia.");
+        }
+    }
+
+    public void mostrarStatus(int pontuacao, int tentativas, int tesourosEncontrados, int armadilhasEncontradas,
+            int vaziosEncontrados) {
         System.out.println("Status do Jogo: ");
-        System.out.println("Pontuação " + pontuação +  " pontos");
-        System.out.println("Tentativas restanrtes: ");
-        System.out.println("Tesouros encontrados: ");
-        System.out.println("Armadilhas encontradas: ");
-        System.out.println("Posições vazias exploradas: ");
-        
+        System.out.println("Pontuação " + pontuacao + " pontos");
+        System.out.println("Tentativas restanrtes: " + tentativas);
+        System.out.println("Tesouros encontrados: " + tesourosEncontrados);
+        System.out.println("Armadilhas encontradas: " + armadilhasEncontradas);
+        System.out.println("Posições vazias exploradas: " + vaziosEncontrados);
+
     }
-    
-    
-    
-    
 
-    
 }
-
--- Trabalho Facul/Ilha Tesuuro/src/Principal.java
+++ Trabalho Facul/Ilha Tesuuro/src/Principal.java
@@ -4,30 +4,30 @@ public class Principal {
 
     public Principal() {
         Jogo jogo = new Jogo();
-        
         Scanner ler = new Scanner(System.in);
         int ale;
-
-        System.out.println("===== ILHA DOS TESOUROS =====\r\n" + //
-                "1 - Mostrar instruções\r\n" + //
+        
+        do{
+        System.out.println("1 - Mostrar instruções\r\n" + //
                 "2 - Mostrar mapa\r\n" + //
                 "3 - Jogar\r\n" + //
                 "4 - Mostrar status\r\n" + //
                 "5 - Sair");
         ale = ler.nextInt();
 
+        
         switch (ale) {
             case 1:
-                
+                jogo.instrucoes();
                 break;
             case 2:
-
+                jogo.mostrarMapa(null, null);
                 break;
             case 3:
-
+            jogo.jogando(ler);
                 break;
             case 4:
-
+                jogo.mostrarStatus(ale, ale, ale, ale, ale);
                 break;
             case 5:
                 System.out.println("Sistema encerrado.");
@@ -38,6 +38,9 @@ public class Principal {
                 break;
         }
 
+        
+        } while( ale != 5);
+        
     }
 
     public static void main(String[] args) {
